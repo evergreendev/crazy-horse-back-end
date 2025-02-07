748a5033a320763976c47a04925f546cd138180f
Joe simpson <joe@egmrc.com>
2025-02-07 15:54:43 -0700

    Fix email nonsense

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -53,8 +53,7 @@ export default buildConfig({
         bundler: webpackBundler(),
     },
     cors: [
-        process.env.PAYLOAD_PUBLIC_NEXT_URL,
-        process.env.PAYLOAD_PUBLIC_SERVER_URL
+        "*"
     ],
     csrf: [
         process.env.PAYLOAD_PUBLIC_NEXT_URL,
